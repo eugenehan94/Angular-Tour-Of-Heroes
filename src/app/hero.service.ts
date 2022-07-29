e927af7ef1b16d10019798aeda7acf979d8a813d
eugenehan94 <eugene.han94@gmail.com>
2022-07-29 19:18:10 -0400

    added angular router and refactoring

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,18 +4,33 @@ import { Observable, of } from 'rxjs'
 import { MessageService } from './message.service'
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes'
-
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+   return this.http.get<Hero[]>(this.heroesUrl)
   }
+
+  getHero(id: number): Observable<Hero> {
+      // For now, assume that a hero with the specified `id` always exists.
+  // Error handling will be added in the next step of the tutorial.
+
+  const hero = HEROES.find(h => h.id === id)!;
+  this.messageService.add(`HeroService: fetch hero id = ${id}` );
+  return of(hero)
+  }
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,24 +10,13 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: "Windstorm"
-  // };
-  // heroes = HEROES;
-  selectedHero?: Hero;
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero= hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
